d665bb03886c1f18cf6245e8884f128d47d731dc
Seungchan Lee <seunggs@gmail.com>
2024-05-02 19:08:13 -0700

    WIP

--- src/scripts/release.ts
+++ src/scripts/release.ts
@@ -1,22 +1,28 @@
 import { $ } from 'bun'
 
 async function main() {
-  // Build
-  const tempBuildFilePath = './temp/sidetrek'
-  await $`npm version patch && bun build ./index.ts --compile --minify --sourcemap --outfile ${tempBuildFilePath}`
-  console.log('Packaged built successfully.')
+  try {
+    // Build
+    const tempBuildFilePath = './temp/sidetrek'
+    await $`npm version patch && bun build ./index.ts --compile --minify --sourcemap --outfile ${tempBuildFilePath}`
+    console.log('Packaged built successfully.')
+  } catch (error) {
+    console.error('Error building package:', error)
+  }
 
-  const version = await $`cat package.json | jq -r '.version'`.text()
-  console.log(`version=${version}`)
+  try {
+    const version = await $`cat package.json | jq -r '.version'`.text()
 
-  // Copy the release script
-  await $`cp ./src/scripts/install.sh ${tempBuildFilePath}`
-  console.log('Copied release script.')
+    // Copy the release script
+    await $`cp ./src/scripts/install.sh ${tempBuildFilePath}`
+    console.log('Copied release script.')
 
-  // Tar
-  const tarResp = await $`tar -czvf sidetrek.${version}.tar.gz ${tempBuildFilePath}`.text()
-  console.log(tarResp)
-  console.log('Tar created successfully.')
+    // Tar
+    await $`tar -czvf sidetrek.${version}.tar.gz ${tempBuildFilePath}`
+    console.log('Tar created successfully.')
+  } catch (error) {
+    console.error('Error creating tar:', error)
+  }
 
   // Clean up
   // await $`rm -rf ${tempBuildFilePath}`.text()
