81fdc982790f10d77d4573988f57d36765f9ebf1
Seungchan Lee <seunggs@gmail.com>
2024-05-02 19:04:40 -0700

    WIP

--- src/scripts/release.ts
+++ src/scripts/release.ts
@@ -4,15 +4,19 @@ async function main() {
   // Build
   const tempBuildFilePath = './temp/sidetrek'
   await $`npm version patch && bun build ./index.ts --compile --minify --sourcemap --outfile ${tempBuildFilePath}`
+  console.log('Packaged built successfully.')
+
+  const version = await $`cat package.json | jq -r '.version'`.text()
+  console.log(`version=${version}`)
 
   // Copy the release script
   await $`cp ./src/scripts/install.sh ${tempBuildFilePath}`
+  console.log('Copied release script.')
 
   // Tar
-  const version = await $`cat package.json | jq -r '.version'`.text()
-  console.log(`version=${version}`)
   const tarResp = await $`tar -czvf sidetrek.${version}.tar.gz ${tempBuildFilePath}`.text()
   console.log(tarResp)
+  console.log('Tar created successfully.')
 
   // Clean up
   // await $`rm -rf ${tempBuildFilePath}`.text()
