45d0efe0aaa7ebc3c4f64bbea583bb1570e6d2ef
Seungchan Lee <seunggs@gmail.com>
2024-06-13 14:03:15 -0700

    remove windows

--- src/scripts/release.ts
+++ src/scripts/release.ts
@@ -20,7 +20,7 @@ const { values: options, positionals } = parseArgs({
   allowPositionals: true,
 })
 
-const availableArchs = ['linux-x64', 'linux-arm64', 'windows-x64', 'darwin-x64', 'darwin-arm64'] as const
+const availableArchs = ['linux-x64', 'linux-arm64', 'darwin-x64', 'darwin-arm64'] as const
 type Arch = (typeof availableArchs)[number]
 const archOption = options.arch as Arch | undefined
 
